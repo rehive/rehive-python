6f6f500ae6c2a17c6bec66ed930e199a70b70599
ConnorMac <cmacdougall78@gmail.com>
2017-07-12 11:51:17 +0200

    Increment release version

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='rehive',
-    version='0.2.5',
+    version='0.3.5',
     description='Rehive SDK for Python',
     long_description=long_description,
     url='https://github.com/rehive/rehive-python',
