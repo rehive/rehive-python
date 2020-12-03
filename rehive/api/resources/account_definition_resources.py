fb395172731005a6c4fafd61dd7e05a2ee50c8ae
OdinsPlasmaRifle <generalaetius.jbvb@gmail.com>
2020-12-03 15:30:15 +0200

    Fixed minor spelling errors

--- rehive/api/resources/account_definition_resources.py
+++ rehive/api/resources/account_definition_resources.py
@@ -4,7 +4,7 @@ from .base_resources import Resource, ResourceCollection, ResourceList
 class APIAccountDefinitions(ResourceList, ResourceCollection):
     def __init__(self, client, endpoint='', filters=None, resource_identifier=None):
         self.resources = (APIAccountDefinitionGroups,)
-        super(APIAccountDefinitionss, self).__init__(client, endpoint, filters)
+        super(APIAccountDefinitions, self).__init__(client, endpoint, filters)
 
     @classmethod
     def get_resource_name(cls):
